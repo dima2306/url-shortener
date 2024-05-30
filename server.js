e2ddabe8bb4148c13e0033a5d875dfdb7b2e448d
dima23 <dima.simonishvili@gmail.com>
2024-05-30 14:26:34 +0400

    Adds MongoDB connection.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // Listen to requests
 app.listen(3000);
@@ -12,6 +13,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // Register morgan as logger
 app.use(morgan('dev'));
+
+// Connect to MongoDB
+console.log('Connecting to MongoDB...');
+mongoose.connect(process.env.DB_URI)
+    .then(response => console.log('Connected to MongoDB.'))
+    .catch(err => console.error(err));
 
 // Base routes
 app.get('/', (req, res) => {
