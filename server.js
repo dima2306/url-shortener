2e0d2de754a319a0a48e7652e60f7d097f8ea3bd
dima23 <dima.simonishvili@gmail.com>
2024-06-15 18:21:23 +0400

    Casts app port strictly to number.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const PORT = Number.parseInt(process.env.PORT) || 3000;
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
