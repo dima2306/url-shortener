44ae92ae8dcd356cfe12a7e86e1fa857379b08f2
dima23 <dima.simonishvili@gmail.com>
2024-06-15 19:33:25 +0400

    Adds multer dependency to handle FormData request. Updates create endpoint to accept multipart form data.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = Number.parseInt(process.env.PORT) || 3000;
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer();
 const URL = require('./models/Url');
 const shortenUrlService = require('./services/ShortenUrlService');
 const helpers = require('./helpers/helper')
@@ -16,6 +18,7 @@ app.use(express.static('public'));
 // Register morgan as logger
 app.use(morgan('dev'));
 
+app.use(express.urlencoded({ extended: true }));
 // Connect to MongoDB
 console.log('Connecting to MongoDB...')
 mongoose.connect(process.env.DB_URI).then(() => {
@@ -29,9 +32,20 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/create', async (req, res) => {
-    if (helpers.isObjectEmpty(req.query)) {
-        res.status(400).json({data: {type: 'error', message: 'Request is empty. Please fill the required fields.'}});
+// upload.none() is a middleware function that processes the FormData but does not handle any files.
+// The processed data is stored in req.body.
+app.post('/create', upload.none(), async (req, res) => {
+    if (helpers.isObjectEmpty(req.body)) {
+        res.status(400).json({
+            data: {
+                type: 'error',
+                message: 'Request is empty. Please fill the required fields.',
+            },
+        })
+    }
+
+    if (req.body.originalUrl === '') {
+        res.status(400).json({ data: { type: 'error', message: 'Original URL is required.' } })
     }
 
     const url = new URL({
