4e55cb89a5cf87a3ca38e9584590f2deb7cfc565
Dimitri <dima.simonishvili@gmail.com>
2024-10-26 22:57:43 +0400

    Code formatting.
    Signed-off-by: Dimitri <dima.simonishvili@gmail.com>

--- services/ShortenUrlService.js
+++ services/ShortenUrlService.js
@@ -4,22 +4,26 @@ const UrlModel = require('../models/Url');
 const {isURL} = require('validator');
 
 module.exports = {
-    shortenUrl: async function (originalUrl) {
-        if (!isURL(originalUrl)) {
-            throw new Error('Invalid URL format.');
-        }
+  shortenUrl: async function(originalUrl) {
+    if (!isURL(originalUrl)) {
+      throw new Error('Invalid URL format.');
+    }
 
-        let shortenedUrl;
-        let isUnique = false;
+    let shortenedUrl;
+    let isUnique = false;
 
-        while (!isUnique) {
-            shortenedUrl = Math.random().toString(36).substring(2, 8);
-            const result = await UrlModel.countDocuments({ shortenedUrl: shortenedUrl });
-            if (result === 0) {
-                isUnique = true;
-            }
-        }
+    while (!isUnique) {
+      shortenedUrl = Math.random().toString(36).substring(2, 8);
 
-        return shortenedUrl;
-    },
-}
+      const result = await UrlModel.countDocuments(
+          {shortenedUrl: shortenedUrl},
+      );
+
+      if (result === 0) {
+        isUnique = true;
+      }
+    }
+
+    return shortenedUrl;
+  },
+};
