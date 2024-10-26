9d7788a15859d02caf3ccac458b2c0a5cdfbb8b4
Dimitri <dima.simonishvili@gmail.com>
2024-10-26 22:54:07 +0400

    Adds url validator. Improves code structure.
    Signed-off-by: Dimitri <dima.simonishvili@gmail.com>

--- services/ShortenUrlService.js
+++ services/ShortenUrlService.js
@@ -1,13 +1,23 @@
 'use strict';
 
-const UrlModel = require("../models/Url");
+const UrlModel = require('../models/Url');
+const {isURL} = require('validator');
 
 module.exports = {
     shortenUrl: async function (originalUrl) {
-        const shortenedUrl = Math.random().toString(36).substring(2, 8);
-        const result = await UrlModel.countDocuments({shortenedUrl: shortenedUrl});
-        if (result > 0) {
-            return this.shortenUrl(originalUrl);
+        if (!isURL(originalUrl)) {
+            throw new Error('Invalid URL format.');
+        }
+
+        let shortenedUrl;
+        let isUnique = false;
+
+        while (!isUnique) {
+            shortenedUrl = Math.random().toString(36).substring(2, 8);
+            const result = await UrlModel.countDocuments({ shortenedUrl: shortenedUrl });
+            if (result === 0) {
+                isUnique = true;
+            }
         }
 
         return shortenedUrl;
