0c2dd5f42768e1ba6e4e9ea5a9ba7cec16c7807d
Dimitri <dima.simonishvili@gmail.com>
2024-11-15 02:17:37 +0400

    Adds user registration validation rules.

---
+++ validations/UserRegistrationValidation.js
@@ -0,0 +1,26 @@
+const {body} = require('express-validator');
+
+const registerUserRules = [
+  body('name')
+    .escape().trim()
+    .notEmpty().withMessage('Name is required.')
+    .isLength({min: 3, max: 30}).withMessage('Name is too short.'),
+
+  body('surname')
+    .escape().trim()
+    .notEmpty().withMessage('Surname is required.')
+    .isLength({min: 4, max: 40}).withMessage('Surname is too short.'),
+
+  body('email')
+    .escape().trim()
+    .notEmpty().withMessage('Email is required.')
+    .isEmail().withMessage('Invalid email address.'),
+
+  body('password')
+    .escape().trim()
+    .notEmpty().withMessage('Password is required.')
+    .isLength({min: 8}).withMessage('Password is too short.'),
+
+];
+
+module.exports = registerUserRules;
