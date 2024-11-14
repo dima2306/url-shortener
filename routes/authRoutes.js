86109d222f182b574247d4c3e0addd5eed5d10b7
Dimitri <dima.simonishvili@gmail.com>
2024-11-15 02:18:13 +0400

    Adds auth routes.

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+const {Router} = require('express');
+const authController = require('../controllers/authController');
+const registerUserRules = require('../validations/UserRegistrationValidation');
+const {validationResult} = require('express-validator');
+
+const router = Router();
+
+router.get('/login', authController.createLogin);
+router.post('/login', authController.storeLogin);
+
+router.get('/register', authController.createRegister);
+router.post('/register', registerUserRules, (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('layout', {
+      content: 'auth/register',
+      errors: errors.array(),
+      oldInput: req.body,
+    });
+  }
+  next();
+}, authController.storeRegister);
+
+module.exports = router;
