2988a5927989b0ed8351b3d644a1f41cae53d9fc
Dimitri <dima.simonishvili@gmail.com>
2024-12-08 14:30:02 +0400

    Adds Mongoose pre-save hook to hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,13 @@ const userSchema = new mongoose.Schema({
     minLength: 8,
   },
 }, {timestamps: true});
+
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
