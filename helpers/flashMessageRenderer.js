44e99bba4b3a32a94734653fabc751922d119b17
Dimitri <dima.simonishvili@gmail.com>
2024-12-14 21:15:00 +0400

    Adds flash message decoupled and independent component.

---
+++ helpers/flashMessageRenderer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const ejs = require('ejs');
+const helpers = require('./helper');
+
+const renderFlashMessage = async (
+  request, messageBag, statusCode = 200, type = 'success') => {
+  request.flash('messageBag', messageBag);
+  return new Promise((resolve, reject) => {
+    ejs.renderFile('views/_partials/flash_message.ejs', {
+      messages: request.flash('messageBag'),
+      helpers: helpers,
+    }, (err, str) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      resolve({type: type, code: statusCode, data: str});
+    });
+  });
+};
+
+module.exports = renderFlashMessage;
