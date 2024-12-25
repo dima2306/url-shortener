0c3d4b02cbc7bff3ea13d44080ade01e650d9b49
Dimitri <dima.simonishvili@gmail.com>
2024-12-25 19:01:50 +0400

    Revert isGuest and user variables to the default values.

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,8 +67,8 @@ function storeRegister(req, res) {
 
 function logOut(req, res) {
   res.clearCookie('jwt');
-  req.session.isGuest = true;
-  req.session.user = null;
+  req.isGuest = true;
+  req.user = null;
 
   res.redirect('/');
 }
